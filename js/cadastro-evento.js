7bb70ff518af501a5fbc9a59d3dfc0f86985c36e
rafec <rafael.brizuena@gmail.com>
2023-03-11 11:05:52 -0400

    direct input data from form submit, tested and it works perfectly

--- js/cadastro-evento.js
+++ js/cadastro-evento.js
@@ -6,31 +6,48 @@ formEl.addEventListener('submit', event => {
     // const formData = new FormData(formEl);
     // const data = new URLSearchParams(formData);
     
-    const data = `{\r\n    \"name\": \"Evento teste 2\",\r\n    \"poster\": \"link da imagem\",\r\n    \"attractions\": [\r\n        \"Cantor 1\"\r\n    ],\r\n    \"description\": \"Evento incrivel\",\r\n    \"scheduled\": \"2022-03-24T00:57:37.761Z\",\r\n    \"number_tickets\": 10\r\n}`
+    // const data = `{\r\n    \"name\": \"Evento teste 2\",\r\n    \"poster\": \"link da imagem\",\r\n    \"attractions\": [\r\n        \"Cantor 1\"\r\n    ],\r\n    \"description\": \"Evento incrivel\",\r\n    \"scheduled\": \"2022-03-24T00:57:37.761Z\",\r\n    \"number_tickets\": 10\r\n}`
+
+    const data = {
+        "name": "Evento teste 99",
+        "poster": "link da imagem",
+        "attractions": [
+            "Cantor 1"
+        ],
+        "description": "Evento incrivel",
+        "scheduled": "2022-03-24T00:57:37.761Z",
+        "number_tickets": 10
+    }
     
     const endpoint = "https://soundgarden-api.vercel.app/events";
     
     fetch(endpoint, {
         method: 'POST',
-        body: data,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
         redirect: 'follow'
     })
     .then(res => res.json())
     .then(data => console.log(data))
     .catch(error => console.log(error));
+
+    console.log(JSON.stringify(data))
 });
 
-//     curl --location 'https://soundgarden-api.vercel.app//events' \
-// --data '{
-    //     "name": "Evento teste 2",
-    //     "poster": "link da imagem",
-    //     "attractions": [
-        //         "Cantor 1"
-        //     ],
-        //     "description": "Evento incrivel",
-        //     "scheduled": "2022-03-24T00:57:37.761Z",
-        //     "number_tickets": 10
-        // }'
+// var raw = "{\r\n    \"name\": \"Evento teste 2\",\r\n    \"poster\": \"link da imagem\",\r\n    \"attractions\": [\r\n        \"Cantor 1\"\r\n    ],\r\n    \"description\": \"Evento incrivel\",\r\n    \"scheduled\": \"2022-03-24T00:57:37.761Z\",\r\n    \"number_tickets\": 10\r\n}";
+
+// var requestOptions = {
+//   method: 'POST',
+//   body: raw,
+//   redirect: 'follow'
+// };
+
+// fetch("https://soundgarden-api.vercel.app//events", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
         
         
         // const data = {
